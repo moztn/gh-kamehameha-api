a1b7b84b10cabd9466a76f7f21eb783e437c544d
Alexandre BM <s@rednaks.tn>
2015-05-30 22:42:06 +0100

    Enable Cross Origin

--- app.py
+++ app.py
@@ -65,5 +65,14 @@ class GhData(Resource):
 
 api.add_resource(GhData, '/')
 
+
+# Cross Origin
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  return response
+
 if __name__ == '__main__':
   app.run(debug=True)
